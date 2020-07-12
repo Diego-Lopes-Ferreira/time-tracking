325ac0ef60ccb9856b021a530bc1453a275c6b31
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-07-12 17:42:23 -0300

    :heavy_plus_sign: Tab and Stack Navigation

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// Pages
+import Home from './src/pages/home';
+import Info from './src/pages/info';
+import Settings from './src/pages/settings';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Info" component={Info} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 
-// import { Container } from './styles';
+import { styles } from '../utils/styles';
 
+const ATest = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => { navigation.navigate('btest') }}>
+        <Text>Go to other page</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+const BTest = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => {navigation.goBack()}}>
+        <Text>Go back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 const Settings = () => {
-  return <View />;
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        gestureDirection: 'horizontal',
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+      }}
+      headerMode='none'
+    >
+      <Stack.Screen name='atest' component={ATest} />
+      <Stack.Screen name='btest' component={BTest} />
+    </Stack.Navigator>
+  );
 }
 
 export default Settings;
